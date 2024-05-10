4ec683e0e722ed77ff6a11c8afcdadb2907af6cc
BlackpearlJack <lestercarlyle@gmail.com>
2024-05-10 12:59:11 +0300

    Updates by user, updates by admin

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,20 +4,35 @@ import { createUser,
     logoutCurrentUser, 
     getAllUsers, 
     getCurrentUserProfile,
-    updateCurrentUserProfile 
+    updateCurrentUserProfile,
+    deleteUserById, 
+    getUserById,
+    updateUserById
 } from '../controllers/userController.js';
 import { authenticate, authorizeAdmin } from '../middlewares/authMiddleware.js';
 
 
 const router = express.Router();
 
-router.route('/').post(createUser).get(authenticate, authorizeAdmin, getAllUsers);
-router.post('/auth', loginUser);
-router.post('/logout', logoutCurrentUser)
+router
+.route('/')
+.post(createUser)
+.get(authenticate, authorizeAdmin, getAllUsers);
+router
+.post('/auth', loginUser);
+router
+.post('/logout', logoutCurrentUser)
 
-router.route('/profile').get(authenticate, getCurrentUserProfile).
-put(authenticate, updateCurrentUserProfile)
+router
+.route('/profile')
+.get(authenticate, getCurrentUserProfile)
+.put(authenticate, updateCurrentUserProfile)
 
+// Admin routes 👇
+router.route('/:id')
+.delete(authenticate, authorizeAdmin, deleteUserById)
+.get(authenticate, authorizeAdmin, getUserById)
+.put(authenticate, authorizeAdmin, updateUserById)
 
 
 export default router;
